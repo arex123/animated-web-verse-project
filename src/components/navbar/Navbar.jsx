8fc2cc520019c48c62f542bdbc5d6b97db45ab8e
arex123 <ad47kumar@gmail.com>
2024-06-09 22:49:44 +0530

    sidebar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./navbar.scss";
 const Navbar = () => {
-  const [openSidebar,setOpenSidebar] = useState(false)
+  const [openSidebar, setOpenSidebar] = useState(false);
+  console.log(openSidebar)
+
+  function calling(){
+    console.log("hi")
+  }
   return (
     <div className="navbar">
       <div className="sidebar">
-        <div className="menu-open">
+        <div onClick={() => setOpenSidebar(!openSidebar)} className="menu-open" >
           <div>
             <svg
               width="28"
@@ -31,25 +36,99 @@ const Navbar = () => {
               />
             </svg>
           </div>
-          <div onClick={()=>setOpenSidebar(true)} className="sidebar-drawer">
-            <div className="sidebar-drawer_left">
-              <div>
-                <li className="discover-text">DISCOVER</li>
-                <ul>
-                  <li>Story</li>
-                  <li>protocol</li>
-                  <li>journal</li>
-                  <li>media</li>
-                  <li>gallary</li>
-                  <li>about</li>
-                </ul>
+          {openSidebar && (
+            <div className="sidebar-drawer">
+              <div className="sidebar-drawer_left">
+                <div>
+                  <span className="discover-text-1">DISCOVER</span>
+                  <ul className="sidebar-menu">
+                    <li>Story</li>
+                    <li>protocol</li>
+                    <li>journal</li>
+                    <li>media</li>
+                    <li>gallary</li>
+                    <li>about</li>
+                  </ul>
+                </div>
+                <div>
+                  <span className="discover-text-2">Connect</span>
+                  <ul className="connect-dis">
+                    <li>Twitter</li>
+                    <li>Discord</li>
+                  </ul>
+                </div>
+                <div>
+                  <span className="discover-text-3">Buy on</span>
+                  <ul className="connect-dis">
+                    <li>Opensa</li>
+                  </ul>
+                </div>
+                <div>
+                  <span>US-en</span>
+                  <span className="connect-dis-date">@ 2022</span>
+                </div>
               </div>
-              <div></div>
-              <div></div>
-              <div></div>
+              <div className="sidebar-drawer_right">
+                <div onClick={() => setOpenSidebar(!openSidebar)} className="sidebar-drawer_right_close-x">
+                  <span id="cross-line-1"></span>
+                  <span id="cross-line-2"></span>
+                </div>
+                <div className="sidebar-drawer_right_middle-icon">                  
+                  <svg
+                    width="48"
+                    height="75"
+                    viewBox="0 0 48 75"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M24.0039 25.5531V0.000567823"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M24.0098 74.3919V48.8394"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M27.748 25.458L27.7515 30.2442L27.6869 35.687"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M27.748 38.6104L27.7515 43.3966L27.6869 48.8393"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M20.2676 25.458L20.271 30.2442L20.2064 35.687"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M20.2676 38.6104L20.271 43.3966L20.2064 48.8393"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M47.9082 37.1484L28.891 37.1484"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                    <path
+                      d="M19.0176 37.1484L0.000392523 37.1484"
+                      stroke="#FFFCFC"
+                      stroke-width="3.5"
+                    />
+                  </svg>
+                </div>
+                <div className="sidebar-drawer_right_close_music-toggle">
+                  on
+                </div>
+              </div>
             </div>
-            <div className="sidebar-drawer__right"></div>
-          </div>
+          )}
         </div>
         <div className="sidebar-middle-icon">
           <svg
