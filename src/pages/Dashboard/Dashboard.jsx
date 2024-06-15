d3b0ce644ec5d44c61a9b6df0c19f04b01afb56e
arex123 <ad47kumar@gmail.com>
2024-06-16 01:32:12 +0530

    screen-1 complete

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,46 +1,42 @@
-import './dashboard.scss'
+import "./dashboard.scss";
 import { gsap } from "gsap";
-import { ScrollTrigger } from 'gsap/all';
-import { useEffect } from 'react';
+import { ScrollTrigger } from "gsap/all";
+import { useEffect } from "react";
 
-gsap.registerPlugin(ScrollTrigger) 
-
+gsap.registerPlugin(ScrollTrigger);
 
 const Dashboard = () => {
-
-
-  useEffect(()=>{
-
+  useEffect(() => {
     window.addEventListener("mousemove", (event) => {
       let width = window.innerWidth,
-      height = window.innerHeight,
-      positionX = event.clientX / width - 0.55,
-      positionY = event.clientY / height - 0.55;
-      
+        height = window.innerHeight,
+        positionX = event.clientX / width - 0.55,
+        positionY = event.clientY / height - 0.55;
+
       gsap.to(".back-img-hero1", {
         rotationX: -positionX * 18,
         rotationY: positionY * 18,
-        transformPerspective:1000,
+        transformPerspective: 1000,
       });
       // gsap.to(".girl-img", {
-        //   rotationX: -positionX * 18,
-        //   rotationY: positionY * 18,
-        //   // transformPerspective:1000,
-        // });
-      });
-      
+      //   rotationX: -positionX * 18,
+      //   rotationY: positionY * 18,
+      //   // transformPerspective:1000,
+      // });
+    });
+
     const { innerHeight } = window;
 
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".back-img-hero1",
         // pin: true,
-        start:'0% 5%',
-        end:'center 30%',
+        start: "0% 10%",
+        end: "center 30%",
 
         // end:`+=${innerHeight*2}`,
         scrub: true,
-        markers:true
+        // markers:true
         // markers: {
         //   startColor: "red",
         //   endColor: "red",
@@ -61,24 +57,24 @@ const Dashboard = () => {
       scale: 10,
       transformOrigin: "bottom 40%",
       // stager: 0.25,
-      duration: 1,      
+      duration: 1,
     });
-    
-    console.log('innerHeight ',innerHeight)
+
+    console.log("innerHeight ", innerHeight);
     // tl.to(".back-img-hero1", {
     //   duration: 1,
     //   yPercent: 20,
-      // marginTop:'10%',
-      // start: `+=${innerHeight / 2}`,
-      // end: `+=${innerHeight / 2}`,
-      // marker:true
-      // scrub: true,
+    // marginTop:'10%',
+    // start: `+=${innerHeight / 2}`,
+    // end: `+=${innerHeight / 2}`,
+    // marker:true
+    // scrub: true,
     // });
     // tl.set(".back-img-hero", {
     //   // duration: 1,
     //   position:'fixed !important',
     //   zIndex: 999,
-     
+
     // });
 
     // tl.set(".back-img-hero",{
@@ -94,32 +90,78 @@ const Dashboard = () => {
     //   width:"20%",
     // })
 
-  },[])
+    // paragraph animation onScroll
+    let screen1TL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".screen-1",
+        // toggleActions: "restart pause none none",
+        start: "10% 10%",
+        end: "bottom 95%",
+        markers: false,
+        scrub: true,
+      },
+    });
+
+    screen1TL.to(".screen-1", {
+      opacity: 0,
+      height:0,
+      display:'none'
+    },);
+
+    // screen 2
+    let screen2TL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".screen-2",
+        start: "10% 10%",
+        end: "bottom 95%",
+        markers: false,
+        scrub: true,
+      },
+    });
+
+    screen2TL.to(".screen-2", {
+      display:'block',
+    },);
+
+
+  }, []);
 
   return (
-    <div className='dashboard'>
-
+    <div className="dashboard">
       <div className="back-img-hero1">
-        <img className='back-img-hero-pic' src="https://pbs.twimg.com/media/GP6D5CyXkAAIUKO?format=jpg&name=4096x4096" alt="img" />
+        <img
+          className="back-img-hero-pic"
+          src="https://pbs.twimg.com/media/GP6D5CyXkAAIUKO?format=jpg&name=4096x4096"
+          alt="img"
+        />
       </div>
       <div className="screen-1">
-        {/* <p className='paragraph-1'>KPR is a brand that focuses on collective narrative and empowering storytellers. Keepers is a living story, an uncharted world waiting to be explored, to be imagined.</p>
-        <div className='paragraph-2'>
-          <p className='paragraph-2-keep'>Keep.</p>
-          <p className='paragraph-2-protect'>protect.</p>
-          <p className='paragraph-2-reimagine'>reimagine.</p>
-        </div> */}
-       </div>
-      
-      <div className="screen-2"></div>
+        <p className="paragraph-1">
+          KPR is a brand that focuses on collective narrative and empowering
+          storytellers. Keepers is a living story, an uncharted world waiting to
+          be explored, to be imagined.
+        </p>
+        <div className="paragraph-2">
+          <p className="paragraph-2-keep">Keep.</p>
+          <p className="paragraph-2-protect">protect.</p>
+          <p className="paragraph-2-reimagine">reimagine.</p>
+        </div>
+      </div>
+
+      <div className="screen-2">
+        <h2>
+          <p>A familiar world... Set</p>
+          <p>on a different path.</p>
+          <p>on a different path.</p>
+        </h2>
+      </div>
       <div className="screen-3"></div>
       <div className="screen-4"></div>
-      <div className="screen-4"></div> 
       <div className="screen-4"></div>
       <div className="screen-4"></div>
-      
+      <div className="screen-4"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
