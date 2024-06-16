65b81a2180d31f1235b49ee47e002bfd3bbe09e2
arex123 <ad47kumar@gmail.com>
2024-06-17 01:24:15 +0530

    screen -2

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,17 +7,31 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Dashboard = () => {
   useEffect(() => {
+    let cardH = window.screen.availHeight;
+    let cardW = window.screen.availWidth;
     window.addEventListener("mousemove", (event) => {
-      let width = window.innerWidth,
-        height = window.innerHeight,
-        positionX = event.clientX / width - 0.55,
-        positionY = event.clientY / height - 0.55;
+      let centerX = 0 + cardW / 2;
+      let centerY = 0 + cardH / 2;
+
+      let mouseX = event.clientX - centerX;
+      let mouseY = event.clientY - centerY;
+
+      console.log("x", mouseX, mouseY);
+
+      let rotateX = (-1 * 15 * mouseY) / (cardH / 2);
+      let rotateY = (15 * mouseX) / (cardW / 2);
 
       gsap.to(".back-img-hero1", {
-        rotationX: -positionX * 18,
-        rotationY: positionY * 18,
+        rotationX: rotateX,
+        rotationY: rotateY,
         transformPerspective: 1000,
       });
+
+      // let width = window.innerWidth,
+      //   height = window.innerHeight,
+      //   positionX = event.clientX / width - 0.55,
+      //   positionY = event.clientY / height - 0.55;
+
       // gsap.to(".girl-img", {
       //   rotationX: -positionX * 18,
       //   rotationY: positionY * 18,
@@ -25,76 +39,51 @@ const Dashboard = () => {
       // });
     });
 
+    // const card = document.querySelector(".card");
+
+    // card.addEventListener("mousemove", cardMouseMove);
+
+    // function cardMouseMove(event) {
+    //   let cardH = card.offsetHeight;
+    //   let cardW = card.offsetWidth;
+
+    //   let centerX = card.offsetLeft + cardW / 2;
+    //   let centerY = card.offsetTop + cardH / 2;
+
+    //   let mouseX = event.clientX - centerX;
+    //   let mouseY = event.clientY - centerY;
+
+    //   console.log("x", mouseX, mouseY);
+
+    //   let rotateX = (-1 * 15 * mouseY) / (cardH / 2);
+    //   let rotateY = (15 * mouseX) / (cardW / 2);
+
+    //   card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    // }
+
     const { innerHeight } = window;
 
     let tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".back-img-hero1",
-        // pin: true,
         start: "0% 10%",
         end: "center 30%",
-
-        // end:`+=${innerHeight*2}`,
         scrub: true,
-        // markers:true
-        // markers: {
-        //   startColor: "red",
-        //   endColor: "red",
-        //   fontSize: "18px",
-        //   fontWeight: "bold",
-        //   indent: 20,
-        // },
       },
     });
-
-    // tl.to('.back-img-hero1',{
-    //   x:600,
-    //   y:200
-
-    // })
 
     tl.from(".back-img-hero-pic", {
       scale: 10,
       transformOrigin: "bottom 40%",
-      // stager: 0.25,
       duration: 1,
     });
 
     console.log("innerHeight ", innerHeight);
-    // tl.to(".back-img-hero1", {
-    //   duration: 1,
-    //   yPercent: 20,
-    // marginTop:'10%',
-    // start: `+=${innerHeight / 2}`,
-    // end: `+=${innerHeight / 2}`,
-    // marker:true
-    // scrub: true,
-    // });
-    // tl.set(".back-img-hero", {
-    //   // duration: 1,
-    //   position:'fixed !important',
-    //   zIndex: 999,
-
-    // });
-
-    // tl.set(".back-img-hero",{
-    //   borderRadius:"50px",
-    //   position:"fixed",
-    // })
-    // gsap.to(".back-img-hero",{
-    //   scrollTrigger:{
-    //     trigger:".back-img-hero",
-    //     toggleActions:"restart pause none none"
-    //   },
-    //   height:"20%",
-    //   width:"20%",
-    // })
 
     // paragraph animation onScroll
     let screen1TL = gsap.timeline({
       scrollTrigger: {
         trigger: ".screen-1",
-        // toggleActions: "restart pause none none",
         start: "10% 10%",
         end: "bottom 95%",
         markers: false,
@@ -104,26 +93,96 @@ const Dashboard = () => {
 
     screen1TL.to(".screen-1", {
       opacity: 0,
-      height:0,
-      display:'none'
-    },);
+      height: 0,
+      display: "none",
+    });
 
     // screen 2
     let screen2TL = gsap.timeline({
       scrollTrigger: {
         trigger: ".screen-2",
-        start: "10% 10%",
-        end: "bottom 95%",
+        start: "55vh top",
+        end: "bottom 75%",
         markers: false,
         scrub: true,
       },
     });
 
     screen2TL.to(".screen-2", {
-      display:'block',
-    },);
+      display: "flex",
+      opacity: 1,
+    });
 
+    gsap.to(".w23Xvfvf", {
+      scrollTrigger: {
+        trigger: ".screen-2",
+        start: "55vh top",
+        end: "bottom 75%",
+        markers: false,
+        scrub: true,
+      },
+      yPercent: -200,
+      opacity: 1,
+    });
+    gsap.to(".w23Xvfvd", {
+      scrollTrigger: {
+        trigger: ".screen-2",
+        start: "55vh top",
+        end: "bottom 75%",
+        markers: false,
+        scrub: true,
+      },
+      yPercent: -320,
+      opacity: 1,
+    });
 
+    gsap.to(".img-desc_screen-2", {
+      scrollTrigger: {
+        trigger: ".screen-2",
+        start: "55vh top",
+        end: "bottom 75%",
+        scrub: true,
+      },
+      yPercent: -50,
+      opacity: 1,
+    });
+
+    // gsap.to('.img_saviour_screen-2',{
+    //   scrollTrigger: {
+    //     trigger: ".screen-2",
+    //     start: "55vh top",
+    //     end: "bottom 75%",
+    //     scrub: true,
+    //     markers:true
+    //   },
+    //   left:0,
+    //   scale:2,
+    //   rotationY:90,
+    //   // transformPerspective:1000,
+    //   opacity:1
+    // })
+    gsap.fromTo(
+      ".img_saviour_screen-2",
+      {
+        scale: 0.5,
+        rotationY: 90,
+        opacity: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: ".screen-2",
+          start: "55vh top",
+          end: "bottom 75%",
+          scrub: true,
+          markers: false,
+        },
+        transformOrigin: "left center",
+        left: 0,
+        scale: 1,
+        rotationY: 0,
+        opacity: 1,
+      }
+    );
   }, []);
 
   return (
@@ -149,11 +208,40 @@ const Dashboard = () => {
       </div>
 
       <div className="screen-2">
-        <h2>
-          <p>A familiar world... Set</p>
-          <p>on a different path.</p>
-          <p>on a different path.</p>
-        </h2>
+        <div className="screen-2-wrap">
+          <div className="title_screen-2">
+            <h2>
+              <div className="w23Xvfvf">
+                <p className="index_title_screen-2">001</p>
+                <span className="eravfdsc">A familiar world... Set</span>
+              </div>
+              <div className="w23Xvfvd">on a different path.</div>
+            </h2>
+          </div>
+
+          <div className="desc_screen-2">
+            <div className="img-desc_screen-2">
+              <img
+                className="img-img-desc_screen-2"
+                src="./images/nature.jpeg"
+                alt="bg-image-nature"
+              />
+            </div>
+            <div className="text-desc_screen-2">
+              Isolated within the New Eden safe zone, you witness humanity
+              struggling to avoid descending into chaos.
+            </div>
+          </div>
+        </div>
+        <div className="saviour_screen-2">
+          <div className="img_saviour_screen-2">
+            <img
+              className="img_img_saviour_screen-2"
+              src="./images/nature.jpeg"
+              alt="bg-image-nature"
+            />
+          </div>
+        </div>
       </div>
       <div className="screen-3"></div>
       <div className="screen-4"></div>
